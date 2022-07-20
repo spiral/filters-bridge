f1bd1875624f0bfa61519255bd54df6f87c0840b
butschster <butschster@gmail.com>
2022-07-20 19:09:29 +0400

    Adds missed interface

---
+++ src/FilterProviderInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Filters;
+
+use Spiral\Filters\Exception\FilterException;
+
+/**
+ * Creates filters on demand based on a given name and input.
+ */
+interface FilterProviderInterface
+{
+    /**
+     * @throws FilterException
+     */
+    public function createFilter(string $name, InputInterface $input): FilterInterface;
+}
